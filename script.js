34388ba37b9dbb75a2945edbf288ea66005954f7
TruongMinh27 <minhnct0319@dhv.edu.vn>
2022-12-21 23:06:46 +0700

    Add Image

---
+++ script.js
@@ -0,0 +1,21 @@
+const canvas = document.getElementById('canvas1')
+const ctx = canvas.getContext("2d")
+
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 600;
+
+const playerImage = new Image();
+
+playerImage.src = 'image/shadow_dog.png';
+const spriteWidth = 575;
+const spriteHeight = 523;
+
+
+function animate(){
+    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    //ctx.fillRect(100,50,100,100)
+    //ctx.drawImage(image,sx,sy,sw,sh,dx,dy,dw.dh)
+    ctx.drawImage(playerImage, spriteWidth, 1 * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight)
+    requestAnimationFrame(animate)
+}
+animate()
